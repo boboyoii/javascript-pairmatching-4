d697d8b7a5abf324205309cadcc3dcba789e45c4
boyeon <boyeon6331@gamil.com>
2026-01-06 14:16:04 +0900

    feat: 존재하는 과정을 입력했는지 확인하는 기능 추가

--- src/utils/Validator.js
+++ src/utils/Validator.js
@@ -1,9 +1,13 @@
-import { MENUS } from '../constants/pairmatching.js';
+import { COURSES, MENUS } from '../constants/pairmatching.js';
 
 const Validator = {
   isValidFuncNum(num) {
     if (!MENUS.includes(num.toUpperCase()))
       throw new Error('[ERROR] 선택한 기능은 없는 기능입니다.');
+  },
+  isValidCourse(course) {
+    if (!Object.keys(COURSES).includes(course))
+      throw new Error('[ERROR] 해당 과정은 없는 과정입니다.');
   },
 };
 
--- src/view/InputView.js
+++ src/view/InputView.js
@@ -1,16 +1,19 @@
 import { Console } from '@woowacourse/mission-utils';
 import { PROGRESS_MESSAGE } from '../constants/messages.js';
+import Validator from '../utils/Validator.js';
 
 const InputView = {
   async readFuncNum() {
     const funcNum = await Console.readLineAsync(PROGRESS_MESSAGE.SELECT_MENU);
+    Validator.isValidFuncNum(funcNum);
     return funcNum.toUpperCase();
   },
   async readCourseLevelMission() {
     const input = await Console.readLineAsync(
       PROGRESS_MESSAGE.INPUT_COURSE_LEVEL_MISSION
     );
-    const [course, level, mission] = input.split(', ');
+    const [course, level, mission] = input.split(',').map((val) => val.trim());
+    Validator.isValidCourse(course);
     return [course, level, mission];
   },
 };
