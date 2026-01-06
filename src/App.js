0d3bd5f0fbf349993f3306b7fd863a117c4c9de8
boyeon <boyeon6331@gamil.com>
2026-01-06 12:55:36 +0900

    feat: setup project

--- src/App.js
+++ src/App.js
@@ -1,15 +1,5 @@
-import LottoController from './controller/LottoController.js';
-
 class App {
-  lottoController;
-
-  constructor() {
-    this.lottoController = new LottoController();
-  }
-
-  async run() {
-    await this.lottoController.run();
-  }
+  async run() {}
 }
 
 export default App;
